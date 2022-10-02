51a55ae5e7a478e8887a0b14da35dffb21b782d0
Schur33 <90901458+Schur33@users.noreply.github.com>
2022-10-02 17:21:11 +0300

    CREATED: Payment page

---
+++ src/components/Payment/PaymentLink.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { useDropdown } from "../../hooks/useDropdown";
+
+export const PaymentLink: React.FC<{}> = () => {
+  const [currentLink, setCurrentLink] = useState<string | null>("TRC20");
+  const { menuState, handleStateChange } = useDropdown();
+
+  const handleClick: React.MouseEventHandler<HTMLLIElement> = (e) => {
+    setCurrentLink((e.target as HTMLElement).textContent);
+    handleStateChange();
+  };
+
+  return (
+    <div className="payment__block">
+      <div className={menuState ? "payment__select active" : "payment__select"}>
+        <p className="payment__label">Выберите сеть:</p>
+        <button className="payment__select-button" onClick={handleStateChange}>
+          {currentLink}
+        </button>
+        <ul className="payment__select-list">
+          {["TRC20", "TRC19"].map((elem) => (
+            <li onClick={handleClick}>{elem}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="payment__sum">
+        <div className="payment__wrapper">
+          <p>Итого к оплате:</p>
+          <div>
+            0,0023513 <span>USDT</span>
+            <img src="./images/usdt.svg" alt="" />
+          </div>
+        </div>
+      </div>
+      <p className="payment__text">
+        Ваш платеж будет подтвержден после 3 подтверждений от сети blockchain.
+        Обычно это происходит в течении 15-20 минут. Затем статус платежа
+        изменится на “Оплачен”.
+      </p>
+      <p className="payment__timer">Платеж будет отменен через: 20:23</p>
+    </div>
+  );
+};
--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,5 +1,15 @@
+import { PaymentLink } from "../components/Payment/PaymentLink";
+import "../components/Payment/payment.scss";
+import { PaymentSum } from "../components/Payment/PaymentSum";
+
 export const PaymentPage: React.FC<{}> = () => {
   return (
-    <div>Payment</div>
-  )
+    <div className="payment">
+      <h4 className="payment__title">Реквизиты для оплаты счета:</h4>
+      <div className="payment__inner">
+        <PaymentLink />
+        <PaymentSum />
+      </div>
+    </div>
+  );
 };
