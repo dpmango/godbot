e5880359759c17ddda3323bae6627b13ea03e91c
Alexander Hmel <can4ik22@gmail.com>
2022-10-30 20:25:37 +0200

    test

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,10 +1,15 @@
 import { PaymentLink } from "../components/Payment/PaymentLink";
 import "../components/Payment/payment.scss";
 import { PaymentSum } from "../components/Payment/PaymentSum";
+import { Helmet } from "react-helmet";
 
 export const PaymentPage: React.FC<{}> = () => {
   return (
     <div className="payment">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Godbot | Payment</title>
+      </Helmet>
       <h4 className="payment__title">Реквизиты для оплаты счета:</h4>
       <div className="payment__inner">
         <PaymentLink />
